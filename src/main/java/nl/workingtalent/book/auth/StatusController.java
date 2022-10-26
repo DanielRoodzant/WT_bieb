85f1b62c4e77abda8406c6116ab9d3f547b7cb33
DanielRoodzant <daniel.rdznt@gmail.com>
2022-10-26 15:46:03 +0200

    auth removed

---
+++ src/main/java/nl/workingtalent/book/auth/StatusController.java
@@ -0,0 +1,19 @@
+//package nl.workingtalent.book.auth;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class StatusController {
+//
+//	@GetMapping("/status")
+//    public String getStatus() {
+//        return "OK";
+//    }
+//
+//	@GetMapping("/status1")
+//    public String getStatus1() {
+//        return "OK1";
+//    }
+//
+//}
